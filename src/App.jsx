0fe5c41e732875d0486204e506c9653bb911221e
sabbir1257 <masabbir1257@gmail.com>
2025-02-11 09:27:58 +0600

    Responsive Navbar using react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Nav from "./components/DaisyNav/DaisyNav"
+import NavBar from "./components/NavBar/NavBar"
+
 function App() {
   return (
     <>
+    {/* <Nav/> */}
+    <NavBar/>
       <h1 className='text-4xl text-center'>Vite + React</h1>
     </>
   )
 }
 
 export default App
-
